4760ad77fe79810cebfc5685c3d33238ef1f9895
xescugc <xescugil@gmail.com>
2020-10-17 12:52:23 +0200

    service/endpoint: Add wall tests
    Basic test to check the Req/Resp logic

---
+++ service/endpoint/wall_test.go
@@ -0,0 +1,144 @@
+package endpoint_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xescugc/chaoswall/mock"
+	"github.com/xescugc/chaoswall/service/endpoint"
+	"github.com/xescugc/chaoswall/wall"
+)
+
+func TestMakeGetWalls(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		var (
+			ctrl    = gomock.NewController(t)
+			service = mock.NewService(ctrl)
+			ctx     = context.Background()
+			w       = wall.Wall{Name: "Name"}
+			gCan    = "gym-can"
+			req     = endpoint.GetWallsRequest{
+				GymCanonical: gCan,
+			}
+			eresp = endpoint.GetWallsResponse{
+				Walls: []*wall.Wall{&w},
+			}
+		)
+
+		service.EXPECT().GetWalls(ctx, gCan).Return([]*wall.Wall{&w}, nil)
+
+		ep := endpoint.MakeGetWalls(service)
+		resp, err := ep(ctx, req)
+		require.Nil(t, err)
+		assert.Equal(t, eresp, resp)
+	})
+}
+
+func TestMakeCreateWall(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		var (
+			ctrl    = gomock.NewController(t)
+			service = mock.NewService(ctrl)
+			ctx     = context.Background()
+			w       = wall.Wall{Name: "Name"}
+			gCan    = "gym-can"
+			req     = endpoint.CreateWallRequest{
+				GymCanonical: gCan,
+
+				Name: w.Name,
+			}
+			eresp = endpoint.CreateWallResponse{
+				Wall: w,
+			}
+		)
+
+		service.EXPECT().CreateWall(ctx, gCan, w).Return(&w, nil)
+
+		ep := endpoint.MakeCreateWall(service)
+		resp, err := ep(ctx, req)
+		require.Nil(t, err)
+		assert.Equal(t, eresp, resp)
+	})
+}
+
+func TestMakeGetWall(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		var (
+			ctrl    = gomock.NewController(t)
+			service = mock.NewService(ctrl)
+			ctx     = context.Background()
+			w       = wall.Wall{Name: "Name", Canonical: "wall-can"}
+			gCan    = "gym-can"
+			req     = endpoint.GetWallRequest{
+				GymCanonical:  gCan,
+				WallCanonical: w.Canonical,
+			}
+			eresp = endpoint.GetWallResponse{
+				Wall: w,
+			}
+		)
+
+		service.EXPECT().GetWall(ctx, gCan, w.Canonical).Return(&w, nil)
+
+		ep := endpoint.MakeGetWall(service)
+		resp, err := ep(ctx, req)
+		require.Nil(t, err)
+		assert.Equal(t, eresp, resp)
+	})
+}
+
+func TestMakeUpdateWall(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		var (
+			ctrl    = gomock.NewController(t)
+			service = mock.NewService(ctrl)
+			ctx     = context.Background()
+			gCan    = "gym-can"
+			ew      = wall.Wall{Name: "NewName", Canonical: "wall-can"}
+			uw      = wall.Wall{Name: "NewName"}
+			oldCan  = "old-can"
+			req     = endpoint.UpdateWallRequest{
+				GymCanonical:  gCan,
+				WallCanonical: oldCan,
+
+				NewName: uw.Name,
+			}
+			eresp = endpoint.UpdateWallResponse{
+				Wall: ew,
+			}
+		)
+
+		service.EXPECT().UpdateWall(ctx, gCan, oldCan, uw).Return(&ew, nil)
+
+		ep := endpoint.MakeUpdateWall(service)
+		resp, err := ep(ctx, req)
+		require.Nil(t, err)
+		assert.Equal(t, eresp, resp)
+	})
+}
+
+func TestMakeDeleteWall(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		var (
+			ctrl    = gomock.NewController(t)
+			service = mock.NewService(ctrl)
+			ctx     = context.Background()
+			gCan    = "gym-can"
+			w       = wall.Wall{Name: "Name", Canonical: "wall-can"}
+			req     = endpoint.DeleteWallRequest{
+				GymCanonical:  gCan,
+				WallCanonical: w.Canonical,
+			}
+		)
+
+		service.EXPECT().DeleteWall(ctx, gCan, w.Canonical).Return(nil)
+
+		ep := endpoint.MakeDeleteWall(service)
+		resp, err := ep(ctx, req)
+		require.Nil(t, err)
+		assert.Nil(t, resp)
+	})
+}
